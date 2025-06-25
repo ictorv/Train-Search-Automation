0df97c39819492e4a0f13fee26f9246876025b24
ictorv <victor.mygit@gmail.com>
2025-06-25 20:34:41 +0530

    feat: Added SSL Acceptance in Options

--- src/test/java/com/selenium/test/erailUtilities/DriverSetup.java
+++ src/test/java/com/selenium/test/erailUtilities/DriverSetup.java
@@ -4,22 +4,31 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
  
 public class DriverSetup {
 	public WebDriver driver;
 	public static String url = "https://erail.in/";
 	public static String usebrowser;
+	ChromeOptions optionsChrome;
+	EdgeOptions optionsEdge;
 	
 	public WebDriver driverLaunch(String browser) {
 		usebrowser = browser;
 		if (usebrowser.equalsIgnoreCase("chrome")) {
-			driver = new ChromeDriver();
+			optionsChrome = new ChromeOptions();
+			optionsChrome.setAcceptInsecureCerts(true);
+			
+			driver = new ChromeDriver(optionsChrome);
 		}else if (usebrowser.equalsIgnoreCase("edge")) {
-			driver = new EdgeDriver();
-		}else { 
-            throw new IllegalArgumentException("Unsupported browser: " + browser);
-        }
+			optionsEdge = new EdgeOptions();
+			optionsEdge.setAcceptInsecureCerts(true);
+			
+			driver = new EdgeDriver(optionsEdge);
+		}
+		
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
